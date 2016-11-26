87498ca71f01d3cf32201e988e4f828433dfb2b1
Olya Belaya <OlyaBelaya84@gmail.com>
2016-11-26 21:41:03 +0200

    New version

--- Task3/script3.js
+++ Task3/script3.js
@@ -1,20 +1,16 @@
 ﻿// 1. Сумма удвоенных значений каждого элемента
 function sumArray(arr){
-  var num = 0;
-  for (i=0; i<arr.length; i++){
-    num = num + arr[i] * 2;
-  }
+ var num = 0;
+ num = arr.reduce(function(sum, curent){
+       return sum + curent*2;
+ }, 0); 
   return num;
 }
 
 
 // 2. Узнать, есть ли в массиве четные числа
 function evenNumbers(arr){
-  for (i=0; i<arr.length; i++){
-    if (arr[i]%2 || arr[i]<=0) {continue;
-        } else {return true;} 
-  }
-  return false;
+  return arr.some(a => !(a%2) & !(a === 0));
 }
 
 // 3. Соединить элементы массива в одну строку, где слова разделены пробелами
@@ -26,87 +22,55 @@ function strForm(arr){
 // 4. Получить новый массив, в котором каждому элементу будет добавлено число,
 //    равное количеству символов в этой строке
 function newStr(arr){
-  let string = "";
-  let newArr = [];
-  for (i=0; i<arr.length; i++){
-    string = arr[i] + " - " + arr[i].length;
-    newArr.push(string);
-  }
+  let newArr = arr.map(item => `${item} - ${item.length}`);
   return newArr;
 }
 
 // 5. Получить новый массив, в котором все элементы строки содержат
 //    4 или более символов
 function newString(arr){
-  let count = 0;
-  let newArr = [];
-  for (i=0; i<arr.length; i++){
-    count = arr[i].length;
-    if (count >= 4) {
-    newArr.push(arr[i]);
-  }
-  }
+  let newArr = arr.filter(item => item.length >= 4);
   return newArr;
 }
 
 // 6. Получить массив, который будет содержать только не четные числа
 function unEvenNumbersArray(arr){
-  let unEvenArray = [];
-  for (i=0; i<arr.length; i++){
-   if (arr[i]%2) {unEvenArray.push(arr[i]); }
-  }
-  return unEvenArray;
+  return arr.filter(item => item%2);
 }
 
 // 7. Сообщить, является ли сумма всех элементов больше 100 (boolean)
 function sumArrayBoolean(arr){
   let num = 0;
-  for (i=0; i<arr.length; i++){
-    num += arr[i];
-  }
+  arr.forEach(item => num += item);
+  console.log("Sum item = " + num);
   return num > 100 ? true : false;
 }
+
 
 
 // 8. Получить новый массив, в котором все элементы будут отсортированы по кол-ву 
 //    символов в строке по возрастанию
 function newStringSort(arr){
-  let max = 0;
-  let newArr = [];
-  for (var i=0; i<arr.length; i++){
-    if (max < arr[i].length) {
-      max = arr[i].length;
-    } 
-  }
-  
-  for (var l=0; l<= max; l++){
-    for (var j=0; j<arr.length; j++){
-      if (arr[j].length === l) { newArr.push(arr[j]);}
-    }
-  }
-  return newArr.toString();
+  let newArr = arr.slice().sort(function sortNumber(a,b){
+                                 return a.length - b.length;
+                                  });
+  return newArr;
 }
 
 
 // 9. Найти индекс самого длинного слова в массиве
 function index(arr){
-  let max = 0;
-  ind = 0;
-  let newArr = [];
-  for (var i=0; i<arr.length; i++){
-    if (max < arr[i].length) {
-      max = arr[i].length;
-      ind = i;
-    } 
-  }
-  return ind;
+   let newArr = arr.slice().sort(function sortNumber(a,b){
+                                 return a.length - b.length;
+                                  });
+  return arr.findIndex(a => a === newArr[newArr.length-1]);
 }
+
 
 // 10. Получить строку, которая будет содержать все элементы двух массивов,
 //     перечисленных через запятую.
 function newDoubleString(arr1, arr2){
-  let newArr = arr1.concat(arr2);
-  return newArr.toString();
+  return arr1.concat(arr2).toString();
 }
 
 
--- Task4/script4.js
+++ Task4/script4.js
@@ -1,20 +1,20 @@
 ﻿
-function isInArray(item){
-  for (i = 0; i < letters.length; i++){
-    if (item === letters[i]) { return true;}
-  }
-  return false;
+// Написать функцию, которая будет принимать строку (только буквы латинского алфавита) 
+// любой длины и возвращать ее, но удалив из нее все гласные буквы английского алфавита. 
+
+function delItem(item, i, arr){
+  if (letters.includes(item)) {return delete arr[i]}
+  return item;
 }
+
 
 function lettersStr(string){
   let arr = string.split("");
-  for (var i=0; i<=arr.length; i++){ 
-    if (isInArray(arr[i])) {arr.splice(i, 1);  i= i-1;}
-  }
+  arr.forEach(delItem);
   return arr.join("");
 }
 
 var letters = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u", "Y", "y"];
-let str = "ooollkjYYfda";
-let st = lettersStr(str);
-console.log(st);
+let string000 = "ooollkjYYfda";
+let out = lettersStr(string000);
+console.log(out);
